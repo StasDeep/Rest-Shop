13f00ec6ad048647d44bdb89ec6279951c5e0934
stasdeep <glubokiy.stas@gmail.com>
2017-08-26 18:57:16 +0300

    Create models

--- restshop_project/restshop/admin.py
+++ restshop_project/restshop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import (
+    PropertyValue, Property, Product, Tag, Unit
+)
+
 # Register your models here.
+admin.site.register((PropertyValue, Property, Product, Tag, Unit))
--- restshop_project/restshop/models.py
+++ restshop_project/restshop/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+class Property(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class PropertyValue(models.Model):
+    value = models.CharField(max_length=255)
+    property = models.ForeignKey(to=Property, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{}: {}'.format(self.property.name, self.value)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    tag_set = models.ManyToManyField(to=Tag)
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+# TODO: install Pillow
+# class ProductImage(models.Model):
+#     product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    # image = models.ImageField(upload_to="images")
+
+
+class Unit(models.Model):
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    value_set = models.ManyToManyField(to=PropertyValue)
+    sku = models.CharField(max_length=255, primary_key=True)
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        properties = ', '.join(str(value) for value in self.value_set.all())
+        return '{}: {}'.format(self.product.title, properties)
