43ce5abfbc04142bff78955b1064b81694afeacd
stasdeep <glubokiy.stas@gmail.com>
2017-08-27 11:15:09 +0300

    Change ordering on models and make Property name unique

--- restshop_project/restshop/models.py
+++ restshop_project/restshop/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Property(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
@@ -15,12 +15,18 @@ class PropertyValue(models.Model):
     value = models.CharField(max_length=255)
     property = models.ForeignKey(to=Property, on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ['property__name', 'value']
+
     def __str__(self):
         return '{}: {}'.format(self.property.name, self.value)
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ['name']
 
     def __str__(self):
         return self.name
@@ -29,6 +35,9 @@ class Tag(models.Model):
 class Product(models.Model):
     tag_set = models.ManyToManyField(to=Tag)
     title = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ['id']
 
     def __str__(self):
         return self.title
