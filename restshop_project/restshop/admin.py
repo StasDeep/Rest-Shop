2b0c280690b9c8c15a0f8de01bf5699f29851e7e
stasdeep <glubokiy.stas@gmail.com>
2017-08-27 11:01:18 +0300

    Add validation to Unit admin form

--- restshop_project/restshop/admin.py
+++ restshop_project/restshop/admin.py
@@ -1,8 +1,30 @@
+from django import forms
 from django.contrib import admin
+from django.core.exceptions import ValidationError
 
 from .models import (
     PropertyValue, Property, Product, Tag, Unit
 )
 
-# Register your models here.
-admin.site.register((PropertyValue, Property, Product, Tag, Unit))
+
+class UnitForm(forms.ModelForm):
+    class Meta:
+        model = Unit
+        fields = '__all__'
+
+    def clean(self):
+        values = self.cleaned_data.get('value_set')
+        properties = []
+        if values:
+            for value in values.all():
+                if value.property.id not in properties:
+                    properties.append(value.property.id)
+                else:
+                    raise ValidationError('Unit property {} has multiple values'.format(value.property.name))
+        return self.cleaned_data
+
+
+class UnitAdmin(admin.ModelAdmin):
+    form = UnitForm
+
+admin.site.register([PropertyValue, Property, Product, Tag, Unit], UnitAdmin)
