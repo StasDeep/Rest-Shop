02fbe0068a8bc082c70757251ada41759e709ae3
Stas Deep <glubokiy.stas@gmail.com>
2017-09-01 09:21:37 +0300

    Add QuerysetForSellerMixin to make queryset admin filtering more DRY

--- restshop_project/restshop/admin_models.py
+++ restshop_project/restshop/admin_models.py
@@ -1,8 +1,37 @@
 from django import forms
 from django.contrib import admin
+from django.contrib.admin.options import BaseModelAdmin
 from django.core.exceptions import ValidationError
 
 from .models import Seller, Unit, OrderUnit, UnitImage, Product, Order
+
+
+SELLER_LOOKUPS = {
+    'unit': {
+        'lookup': 'product__seller',
+        'model': Unit
+    },
+    'unit_set': {
+        'lookup': 'product__seller',
+        'model': Unit
+    },
+    'unitimage': {
+        'lookup': 'unit_set__product__seller',
+        'model': UnitImage
+    },
+    'product': {
+        'lookup': 'seller',
+        'model': Product
+    },
+    'order': {
+        'lookup': 'unit_set__product__seller',
+        'model': Order
+    },
+    'orderunit': {
+        'lookup': 'unit__product__seller',
+        'model': OrderUnit
+    },
+}
 
 
 def get_seller(request):
@@ -31,39 +60,47 @@ class UnitForm(forms.ModelForm):
         return values
 
 
-class OrderUnitInline(admin.StackedInline):
+class QuerysetForSellerMixin(BaseModelAdmin):
+    """Add restrictions to querysets which are displayed to sellers in foreignkey and manytomany fields."""
+
+    def set_filtered_queryset(self, db_field, request, kwargs):
+        """Change kwargs['queryset'] to show records related only to seller who requests them."""
+        seller = get_seller(request)
+
+        for field in SELLER_LOOKUPS:
+            if db_field.name == field:
+                model = SELLER_LOOKUPS[field]['model']
+                lookup_kwargs = {SELLER_LOOKUPS[field]['lookup']: seller}
+                kwargs['queryset'] = model.objects.filter(**lookup_kwargs).distinct()
+
+        if db_field.name == 'seller':
+            kwargs['queryset'] = Seller.objects.filter(id=seller.id)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if not request.user.is_superuser:
+            self.set_filtered_queryset(db_field, request, kwargs)
+
+        return super(QuerysetForSellerMixin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if not request.user.is_superuser:
+            self.set_filtered_queryset(db_field, request, kwargs)
+
+        return super(QuerysetForSellerMixin, self).formfield_for_manytomany(db_field, request, **kwargs)
+
+
+class OrderUnitInline(QuerysetForSellerMixin, admin.StackedInline):
     model = OrderUnit
     extra = 1
 
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if request.user.is_superuser:
-            return super(OrderUnitInline, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
-        seller = get_seller(request)
-
-        if db_field.name == 'unit':
-            kwargs['queryset'] = Unit.objects.filter(product__seller=seller).distinct()
-
-        return super(OrderUnitInline, self).formfield_for_foreignkey(db_field, request, **kwargs)
-
-
-class UnitImageInline(admin.TabularInline):
+class UnitImageInline(QuerysetForSellerMixin, admin.StackedInline):
     model = UnitImage.unit_set.through
     extra = 1
 
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if request.user.is_superuser:
-            return super(UnitImageInline, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
-        seller = get_seller(request)
-
-        if db_field.name == 'unitimage':
-            kwargs['queryset'] = UnitImage.objects.filter(unit_set__product__seller=seller).distinct()
-
-        return super(UnitImageInline, self).formfield_for_foreignkey(db_field, request, **kwargs)
-
-
-class StaffModelAdmin(admin.ModelAdmin):
+class StaffModelAdmin(QuerysetForSellerMixin, admin.ModelAdmin):
+    """Add restrictions to queryset which are displayed to sellers."""
     seller_field_path = 'seller'
 
     def get_queryset(self, request):
@@ -74,58 +111,30 @@ class StaffModelAdmin(admin.ModelAdmin):
 
         seller = get_seller(request)
 
-        kwargs = {self.seller_field_path: seller}
+        lookup_kwargs = {self.seller_field_path: seller}
 
         # Add distinct to remove duplicates when searching through M2M.
-        return queryset.filter(**kwargs).distinct()
-
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if request.user.is_superuser:
-            return super(StaffModelAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
-
-        seller = get_seller(request)
-
-        if db_field.name == 'product':
-            kwargs['queryset'] = Product.objects.filter(seller=seller)
-
-        if db_field.name == 'unit':
-            kwargs['queryset'] = Unit.objects.filter(product__seller=seller).distinct()
-
-        if db_field.name == 'order':
-            kwargs['queryset'] = Order.objects.filter(unit_set__product__seller=seller).distinct()
-
-        return super(StaffModelAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
-
-    def formfield_for_manytomany(self, db_field, request, **kwargs):
-        if request.user.is_superuser:
-            return super(StaffModelAdmin, self).formfield_for_manytomany(db_field, request, **kwargs)
-
-        seller = get_seller(request)
-
-        if db_field.name == 'unit_set':
-            kwargs['queryset'] = Unit.objects.filter(product__seller=seller)
-
-        return super(StaffModelAdmin, self).formfield_for_manytomany(db_field, request, **kwargs)
+        return queryset.filter(**lookup_kwargs).distinct()
 
 
 class ProductAdmin(StaffModelAdmin):
-    seller_field_path = 'seller'
+    seller_field_path = SELLER_LOOKUPS['product']['lookup']
 
 
 class UnitAdmin(StaffModelAdmin):
+    seller_field_path = SELLER_LOOKUPS['unit']['lookup']
     form = UnitForm
-    seller_field_path = 'product__seller'
     inlines = (UnitImageInline,)
 
 
 class UnitImageAdmin(StaffModelAdmin):
-    seller_field_path = 'unit_set__product__seller'
+    seller_field_path = SELLER_LOOKUPS['unitimage']['lookup']
 
 
 class OrderAdmin(StaffModelAdmin):
-    seller_field_path = 'unit_set__product__seller'
+    seller_field_path = SELLER_LOOKUPS['order']['lookup']
     inlines = (OrderUnitInline,)
 
 
 class OrderUnitAdmin(StaffModelAdmin):
-    seller_field_path = 'unit__product__seller'
+    seller_field_path = SELLER_LOOKUPS['orderunit']['lookup']
