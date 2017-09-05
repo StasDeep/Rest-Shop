8957907a39b11c4e77c8331e9b0a776b5c987032
Stas Deep <glubokiy.stas@gmail.com>
2017-09-05 17:52:06 +0300

    Add pagination for products list

--- restshop_project/ui/app/js/controllers.js
+++ restshop_project/ui/app/js/controllers.js
@@ -1,26 +1,37 @@
 'use strict';
 angular.module('myApp.controllers', [])
-    .controller('SneakersCtrl', ['$scope', '$http', '$location', 'config',
-        function($scope, $http, $location, config) {
+    .controller('SneakersCtrl', ['$scope', '$http', '$location', '$anchorScroll', 'config',
+        function ($scope, $http, $location, $anchorScroll, config) {
             $scope.go = function (path) {
                 $location.path(path);
             };
 
-            $http.get(config.serverUrl + '/products/').then(function (response) {
-                var items = response.data.results;
-                for (var i = 0; i < items.length; i++) {
-                    if (items[i].image === null) {
-                        items[i].image = config.emptyImageUrl;
-                    } else {
-                        items[i].image = config.serverUrl + items[i].image;
+            $scope.changeList = function (apiPath) {
+                $http.get(apiPath).then(function (response) {
+                    var items = response.data.results;
+
+                    for (var i = 0; i < items.length; i++) {
+                        if (items[i].image === null) {
+                            items[i].image = config.emptyImageUrl;
+                        } else {
+                            items[i].image = config.serverUrl + items[i].image;
+                        }
                     }
-                }
-                $scope.sneakersListing = items;
-            });
+
+                    $scope.sneakersListing = items;
+                    $scope.prevPageUrl = response.data.previous;
+                    $scope.nextPageUrl = response.data.next;
+
+                    // Scroll to top of the page to show new results.
+                    $anchorScroll();
+                });
+            };
+
+            $scope.changeList(config.serverUrl + '/products/');
         }
     ])
     .controller('SneakersDetailsCtrl', ['$scope', '$http', '$stateParams', 'config',
-        function($scope, $http, $stateParams, config) {
+        function ($scope, $http, $stateParams, config) {
             $scope.id = $stateParams.id;
 
             $http.get(config.serverUrl + '/products/' + $scope.id).then(function (response) {
