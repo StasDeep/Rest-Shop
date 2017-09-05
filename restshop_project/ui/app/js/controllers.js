baf048c2b6e18275878321895a10d3bad3afb84a
Stas Deep <glubokiy.stas@gmail.com>
2017-09-05 11:57:02 +0300

    Fix units loading and template image URL

--- restshop_project/ui/app/js/controllers.js
+++ restshop_project/ui/app/js/controllers.js
@@ -1,28 +1,30 @@
 'use strict';
 angular.module('myApp.controllers', [])
-    .controller('SneakersCtrl', ['$scope', '$http', '$location', 'config', function($scope, $http, $location, config) {
-        $scope.go = function (path) {
-            $location.path(path);
-        };
+    .controller('SneakersCtrl', ['$scope', '$http', '$location', 'config',
+        function($scope, $http, $location, config) {
+            $scope.go = function (path) {
+                $location.path(path);
+            };
 
-        $http.get(config.serverUrl + '/products/').then(function (response) {
-            var items = response.data.results;
-            for (var i = 0; i < items.length; i++) {
-                if (items[i].image === null) {
-                    items[i].image = config.emptyImageUrl;
-                } else {
-                    items[i].image = config.serverUrl + items[i].image;
+            $http.get(config.serverUrl + '/products/').then(function (response) {
+                var items = response.data.results;
+                for (var i = 0; i < items.length; i++) {
+                    if (items[i].image === null) {
+                        items[i].image = config.emptyImageUrl;
+                    } else {
+                        items[i].image = config.serverUrl + items[i].image;
+                    }
                 }
-            }
-            $scope.sneakersListing = items;
-        });
-    }
+                $scope.sneakersListing = items;
+            });
+        }
     ])
-    .controller('SneakersDetailsCtrl', ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams) {
-        $scope.id = $stateParams.id;
+    .controller('SneakersDetailsCtrl', ['$scope', '$http', '$stateParams', 'config',
+        function($scope, $http, $stateParams, config) {
+            $scope.id = $stateParams.id;
 
-        $http.get(config.serverUrl + '/products/' + $scope.id).then(function (response) {
-            $scope.sneakers = response.data;
-        });
-    }
+            $http.get(config.serverUrl + '/products/' + $scope.id).then(function (response) {
+                $scope.sneakers = response.data;
+            });
+        }
     ]);
