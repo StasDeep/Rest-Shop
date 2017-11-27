95cd7b532b79ab022b222cd4fc337b9bf2653a36
Stas Deep <glubokiy.stas@gmail.com>
2017-11-27 12:55:08 +0300

    Move db logic from serializers to views

--- restshop_project/restshop/api/user/serializers.py
+++ restshop_project/restshop/api/user/serializers.py
@@ -1,9 +1,8 @@
-from django.contrib.auth.models import User, Group, Permission
-from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
-from restshop.api.user.models import Seller, DeliveryInfo
+from restshop.api.user.models import DeliveryInfo
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -20,17 +19,6 @@ class UserSerializer(serializers.ModelSerializer):
             }
         }
 
-    def create(self, validated_data):
-        user = User.objects.create(
-            email=validated_data['email'],
-            username=validated_data['email']
-        )
-
-        user.set_password(validated_data['password'])
-        user.save()
-
-        return user
-
 
 class SellerSerializer(serializers.ModelSerializer):
     name = serializers.CharField(source='seller.name')
@@ -39,52 +27,6 @@ class SellerSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('email', 'password', 'name', 'address')
-
-    def create(self, validated_data):
-        user = User.objects.create(
-            email=validated_data['email'],
-            username=validated_data['email']
-        )
-
-        staff_group = self.get_staff_group()
-        user.groups.add(staff_group)
-
-        user.set_password(validated_data['password'])
-        user.save()
-
-        Seller.objects.create(
-            user=user,
-            name=validated_data['seller']['name'],
-            address=validated_data['seller']['address']
-        )
-
-        return user
-
-    def get_staff_group(self):
-        """Get staff group with seller permissions or create if does not exist."""
-        try:
-            return Group.objects.get(name='Staff')
-        except ObjectDoesNotExist:
-            group = Group.objects.create(name='Staff')
-
-        all_permissions = ('add', 'change', 'delete')
-        content_types = {
-            'unit': all_permissions,
-            'product': all_permissions,
-            'unitimage': all_permissions,
-            'orderunit': all_permissions,
-            'order': ('change',),
-            'property': ('add',),
-            'propertyvalue': ('add',)
-        }
-
-        for content_type in content_types:
-            for permission in content_types[content_type]:
-                codename = '{}_{}'.format(permission, content_type)
-                permission = Permission.objects.get(codename=codename)
-                group.permissions.add(permission)
-
-        return group
 
 
 class DeliveryInfoSerializer(serializers.ModelSerializer):
--- restshop_project/restshop/api/user/views.py
+++ restshop_project/restshop/api/user/views.py
@@ -1,17 +1,30 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group, Permission
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import generics, status
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from restshop.api.user.models import DeliveryInfo
+from restshop.api.user.models import DeliveryInfo, Seller
 from restshop.api.user.serializers import UserSerializer, SellerSerializer, DeliveryInfoSerializer
 
 
-class UserCreateView(generics.CreateAPIView):
+class UserCreateView(APIView):
     permission_classes = (AllowAny,)
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = User.objects.create(
+            email=serializer.validated_data['email'],
+            username=serializer.validated_data['email']
+        )
+
+        user.set_password(serializer.validated_data['password'])
+        user.save()
+
+        return Response(status=status.HTTP_201_CREATED)
 
 
 class UserView(APIView):
@@ -24,10 +37,57 @@ class UserView(APIView):
         return Response(UserSerializer(request.user).data)
 
 
-class SellerCreateView(generics.CreateAPIView):
+class SellerCreateView(APIView):
     permission_classes = (AllowAny,)
-    queryset = User.objects.all()
-    serializer_class = SellerSerializer
+
+    def post(self, request):
+        serializer = SellerSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = User.objects.create(
+            email=serializer.validated_data['email'],
+            username=serializer.validated_data['email']
+        )
+
+        staff_group = self.get_staff_group()
+        user.groups.add(staff_group)
+
+        user.set_password(serializer.validated_data['password'])
+        user.save()
+
+        Seller.objects.create(
+            user=user,
+            name=serializer.validated_data['seller']['name'],
+            address=serializer.validated_data['seller']['address']
+        )
+
+        return user
+
+    def get_staff_group(self):
+        """Get staff group with seller permissions or create if does not exist."""
+        try:
+            return Group.objects.get(name='Staff')
+        except ObjectDoesNotExist:
+            group = Group.objects.create(name='Staff')
+
+        all_permissions = ('add', 'change', 'delete')
+        content_types = {
+            'unit': all_permissions,
+            'product': all_permissions,
+            'unitimage': all_permissions,
+            'orderunit': all_permissions,
+            'order': ('change',),
+            'property': ('add',),
+            'propertyvalue': ('add',)
+        }
+
+        for content_type in content_types:
+            for permission in content_types[content_type]:
+                codename = '{}_{}'.format(permission, content_type)
+                permission = Permission.objects.get(codename=codename)
+                group.permissions.add(permission)
+
+        return group
 
 
 class ChangePasswordView(APIView):
