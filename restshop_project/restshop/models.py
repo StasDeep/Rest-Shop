0600dff7710e645bbc4606f679f736d0af7a3040
Stas Deep <glubokiy.stas@gmail.com>
2018-01-08 12:19:49 +0300

    Add models.py for the app to correcly detect migrations

---
+++ restshop_project/restshop/models.py
@@ -0,0 +1,7 @@
+from restshop.api.cart.models import CartUnit
+from restshop.api.order.models import Order
+from restshop.api.order_unit.models import OrderUnit
+from restshop.api.product.models import Product
+from restshop.api.property.models import Property, PropertyValue
+from restshop.api.tag.models import Tag
+from restshop.api.user.models import Seller, DeliveryInfo
